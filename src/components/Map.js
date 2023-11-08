d7c6d8e87e2795e3a5187ddc4c4139b9f8ad6d78
Sankhasubhra Roy <sankha.asansol@gmail.com>
2023-11-08 22:32:57 +0530

    Create a map component using react-leaflet

---
+++ src/components/Map.js
@@ -0,0 +1,32 @@
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import { Icon } from "leaflet";
+
+const Map = ({ coordinates }) => {
+  const markerIcon = new Icon({
+    iconUrl: "https://cdn-icons-png.flaticon.com/128/684/684908.png",
+    iconSize: [24, 24],
+  });
+
+  return (
+    <div className="h-full w-full">
+      <MapContainer
+        center={coordinates}
+        zoom={13}
+        style={{ height: "100%", width: "100%" }}
+        scrollWheelZoom
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+
+        <Marker position={coordinates} icon={markerIcon}>
+          <Popup>WorkWise</Popup>
+        </Marker>
+      </MapContainer>
+    </div>
+  );
+};
+
+export default Map;
